826eedd50f0b66b4cb5f2e01e41d6ce5e7cb4e89
Sara Anderson <anderson.2934@osu.edu>
2019-01-16 18:53:36 -0500

    Lab 2 completed

---
+++ Fisher.Bookstore/Controllers/AuthorsController.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fisher.Bookstore.Controllers {
+    public class AuthorsController : Controller {
+        
+        public IActionResult Index() {
+            return Content("This is the Books controller's Authors action.");
+        }
+        public IActionResult Featured() {
+            return Content("This is the Books controller's Featured action.");
+        }
+    }
+}
